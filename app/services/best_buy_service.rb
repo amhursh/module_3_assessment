077ad17f8c408640e7ced4a5ff9e2f42d3758d77
Aaron Hursh <amhursh@gmail.com>
2017-11-02 15:23:11 -0600

    Added best buy service with logic

---
+++ app/services/best_buy_service.rb
@@ -0,0 +1,31 @@
+class BestBuyService
+
+  def initialize
+    @api_key = ENV["API_KEY"]
+    @conn = Faraday.new("https://api.bestbuy.com/v1/") do |f|
+      f.adapter Faraday.default_adapter
+    end
+  end
+
+  def show
+    @info = 'storeId,longName,city,distance,storeType,phone'
+  end
+
+  def stores_by_zip(zip)
+    response = conn.get("stores(area(#{zip},25))?format=json&show=storeId,longName,city,distance,storeType,phonee&pageSize=10&apiKey=#{api_key}")
+    JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+  end
+
+  def self.stores_by_zip(zip)
+    new.stores_by_zip(zip)
+  end
+
+  private
+
+    attr_reader :api_key,
+                :show,
+                :conn,
+                :info
+
+end
